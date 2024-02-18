4a1a145f508846b743b275a0c03917e3bac4cfe3
Space Walker <spacedoesrs@gmail.com>
2024-02-18 20:23:01 +0100

    fix class interfaces merger
    it tries to preserve the order of the interfaces but this is not always possible

--- src/main/java/net/fabricmc/stitch/util/StitchUtil.java
+++ src/main/java/net/fabricmc/stitch/util/StitchUtil.java
@@ -101,6 +101,8 @@ public final class StitchUtil {
         int j = 0;
 
         while (i < first.size() || j < second.size()) {
+            int saved = i + j;
+
             while (i < first.size() && j < second.size()
                     && first.get(i).equals(second.get(j))) {
                 out.add(first.get(i));
@@ -115,6 +117,15 @@ public final class StitchUtil {
 
             while (j < second.size() && !first.contains(second.get(j))) {
                 out.add(second.get(j));
+                j++;
+            }
+
+            // if the order is scrambled, it's not possible to merge
+            // the lists while preserving the order from both sides
+            if (i + j == saved) {
+                out.add(first.get(i));
+                out.add(second.get(j));
+                i++;
                 j++;
             }
         }
